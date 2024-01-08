fd9e12fb252419184786f87f7812626a74d069b0
matt <143686761+ulixert@users.noreply.github.com>
2024-01-08 07:06:24 -0800

    feat: add tour schema in postgres and change the tourController

--- src/types/schemas.ts
+++ src/types/schemas.ts
@@ -1,9 +1,5 @@
 import { createInsertSchema } from 'drizzle-zod';
-import { z } from 'zod';
 
 import { tour } from '@/db/schema.js';
 
 export const insertTourSchema = createInsertSchema(tour);
-type Tour = z.infer<typeof insertTourSchema>;
-
-const t: Tour = {};
